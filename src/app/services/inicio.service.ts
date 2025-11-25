3c7d95c0350d85f8eb8b186a650a2e090479913e
ortiz99 <acortiz@softcaribbean.com>
2025-11-24 22:23:48 -0500

    ajuste de env

--- src/app/services/inicio.service.ts
+++ src/app/services/inicio.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
 export class InicioService {
-  servidor = 'http://localhost:5000/api';
+  servidor = environment.apiUrl;
   constructor(private servicio: HttpClient) { }
   calculateZapata(model: any): Observable<any> {
     return this.servicio.post(`${this.servidor}/zapata-combinada`, { model });
